86c1940a2539ddaa3f6e1dd42e6f573bf5bf9dac
Bennett <bsbrabrand@yahoo.com>
2025-02-16 18:27:23 -0500

    test

--- Cookbook10_12.py
+++ Cookbook10_12.py
@@ -1,7 +1,18 @@
 import serial
+import time
 
-ser = serial.Serial('/dev/serial0',9600)
+ser = serial.Serial("/dev/serial0",9600, timeout=1)
 
-ser.write("testing")
+#ser.write(bytes('a', 'utf-8'))
+while(True):
+    if(ser.in_waiting >0):
+        data =ser.readline()
+        print(data)
+
+    
+    #if ser.in_waiting >0:
+     #   print("test2")
+      #  line = ser.readline()
+       # print(line)
 
 #test
