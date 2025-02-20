a3cd152603e1f526787fd11203505df5ea76fb49
Bennett <bsbrabrand@yahoo.com>
2025-02-19 19:20:16 -0500

    add squid

--- Cookbook10_12.py
+++ Cookbook10_12.py
@@ -1,13 +1,17 @@
 import serial
 import time
 
-ser = serial.Serial("/dev/serial0",9600, timeout=1)
+ser = serial.Serial('/dev/serial0',9600)
 
 #ser.write(bytes('a', 'utf-8'))
 while(True):
     if(ser.in_waiting >0):
+        print('e')
         data =ser.readline()
         print(data)
+    #else:
+        #print('a')
+
 
     
     #if ser.in_waiting >0:
